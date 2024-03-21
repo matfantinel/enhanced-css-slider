bc3c428f675cebc806c1f3752612453f997b45ae
Matt Fantinel <matt@fantinel.dev>
2024-03-21 17:03:39 -0300

    Add initial code

--- README.md
+++ README.md
@@ -1,2 +1,136 @@
-# enhanced-css-slider
-Enhanced CSS Slider is a web component that adds some functionality to your existing CSS slider.
+# Enhanced CSS Slider
+
+Enhanced CSS Slider is a web component that wraps up all the JavaScript logic needed to:
+
+- Add Previous/Next arrows to a slider;
+- Add looping functionality;
+
+It ==does not add or alter any markup==, it merely adds behaviors to existing elements.
+
+## How to use
+
+Enhanced CSS Slider assumes you have a scrollable element with the following CSS properties:
+
+```css
+.parent {
+  scroll-snap-type: x mandatory;
+}
+.child {
+  scroll-snap-align: start | center;
+}
+```
+
+More info about scroll-snap can be seen [on this blog post](https://fantinel.dev/css-scroll-snapping).
+
+With that, you can just wrap your scrollable element in the `enhanced-css-slider` component:
+
+```html
+<enhanced-css-slider>
+  <ul class="scrollable-list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+</enhanced-css-slider>
+```
+
+> [!info]
+> Enhanced CSS Slider will look for any `ul` elements inside of it and automatically assume that is the scrollable div that contains the slides. However, in case you need to explicitly tell it what the scrollable element is, you can add the `data-slider-slot="list"` attribute to it.
+
+### Adding previous/next buttons
+
+Since this component doesn't provide any markup, you need to pass in the navigation buttons on your own. Enhanced CSS Slider will then look for elements with the `prev` and `next` classes or `data-slider-slot="prev|next"` attributes and add the event listeners to it.
+
+```html
+<enhanced-css-slider>
+  <ul class="scrollable-list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+
+  <button data-slider-slot="prev">Prev</button>
+  <button data-slider-slot="next">Next</button>
+</enhanced-css-slider>
+```
+
+### Adding a current slider indicator
+
+Similar to prev/next buttons, you can also pass an element with the `current` class or `data-slider-slot="current"` attribute, and Enhanced CSS Slider will automatically set the text of that element to the current slider index.
+
+```html
+<enhanced-css-slider>
+  <ul class="scrollable-list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+
+  <div>
+    Slide <span data-slider-slot="current"></span> of 3
+  </div>
+
+  <button data-slider-slot="prev">Prev</button>
+  <button data-slider-slot="next">Next</button>
+</enhanced-css-slider>
+```
+
+## Props
+
+| Prop Name       | Default | Description                                                                                                                                                   |
+| --------------- | ------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| centered        | false   | Use it if the active slider is center-aligned instead of aligned to the left.                                                                                 |
+| loop            | false   | Whether the slider should loop around. This creates clones of the first/last slides in order to give an illusion that the slides are looping.                 |
+| slides-to-clone | 2       | Only used in `loop` mode. This controls how many slides will be cloned. Increase this number in case the 2 slides aren't enough to give the looping illusion. |
+
+## Slots
+
+| Slot    | Description                                                                                                        |
+| ------- | ------------------------------------------------------------------------------------------------------------------ |
+| list    | The scrollable element. Can be set with `data-slider-slot="list"` or by being a `ul` element.                      |
+| current | The current active index of the slider. Can be set with `data-slider-slot="current"` or with the `current` class.  |
+| prev    | The button that scrolls to the previous slide. Can be set with `data-slider-slot="prev"` or with the `prev` class. |
+| next    | The button that scrolls to the next slide. Can be set with `data-
+
+## Events
+
+| Event        | Params              |
+| ------------ | ------------------- |
+| slideChanged | activeSlide (index) |
+
+## Dealing with JavaScript unavailability
+
+[In cases where JavaScript isn't available](https://www.kryogenix.org/code/browser/everyonehasjs.html), then this component won't be able to render at all. Luckily, since we're using CSS scroll-snap, the slider should still work normally. However, it's a good idea to hide the elements of the page that won't work. You can do that with a `noscript` tag:
+
+```html
+<enhanced-css-slider>
+  <ul class="scrollable-list">
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>
+
+  <div class="progress">
+    Slide <span data-slider-slot="current"></span> of 3
+  </div>
+
+  <div class="navigation">
+    <button data-slider-slot="prev">Prev</button>
+    <button data-slider-slot="next">Next</button>
+  </div>
+
+  <noscript>
+      <!-- Hide JS-only elements if JavaScript is unavailable -->
+      <style>
+          .progress,
+          .navigation {
+            display: none !important;
+          }
+      </style>
+  </noscript>
+</enhanced-css-slider>
+```
+
+## Handling cases where slider is not needed
+
+In situations where there is more than enough space to fit all children in the viewport without scrolling, Enhanced CSS Slider will automatically disable the prev/next arrows and add the `no-scroll` class to your list. You can then use CSS to apply any styling rules you'd like. **This behavior is not present if `centered` or `loop` are true.**
---
+++ src/index.js
@@ -0,0 +1,286 @@
+class EnhancedCssSlider extends HTMLElement {
+  static tagName = 'enhanced-css-slider';
+
+  // Register element so it can be used in the DOM
+  static register(tagName, registry) {
+    if (!registry && 'customElements' in globalThis) {
+      registry = globalThis.customElements;
+    }
+
+    registry?.define(tagName || this.tagName, this);
+  }
+
+  // This is here just for reference
+  // This object is not used
+  static props = {
+    loop: false,
+    centered: false,
+    slidesToClone: 2
+  };
+
+  static events = {
+    slideChanged: 'slideChanged'
+  }
+
+  static classes = {};
+
+  connectedCallback() {
+    /// Make sure we can load web components
+    if (this.shadowRoot || !('replaceSync' in CSSStyleSheet.prototype)) {
+      return;
+    }
+
+    // Create a shadow root
+    let shadowroot = this.attachShadow({ mode: 'open' });
+
+    let slot = document.createElement('slot');
+    shadowroot.appendChild(slot);
+
+    this.content = this;
+    this.content.setAttribute('tabindex', '0');
+    this.content.setAttribute('role', 'region');
+
+    this.props = {};
+
+    this.props.centered = this.content.hasAttribute('centered') && this.content.getAttribute('centered') !== 'false';
+    this.props.loop = this.content.hasAttribute('loop') && this.content.getAttribute('loop') !== 'false';
+    this.props.slidesToClone = this.content.getAttribute('sliders-to-clone') || 2;
+
+    // Get previous and next buttons if they exist
+    this.prev = this.content.querySelector('[data-slider-slot="prev"]') ?? this.content.querySelector('.prev');
+    this.next = this.content.querySelector('[data-slider-slot="next"]') ?? this.content.querySelector('.next');
+
+    // Get current slide indicator if it exists
+    this.currentSlideIndicator = this.content.querySelector('[data-slider-slot="current"]') ?? this.content.querySelector('.current');
+
+    // Try to get what the list of slides is. Look for data-slider-slot=list or ul
+    this.list = this.content.querySelector('[data-slider-slot="list"]') ?? this.content.querySelector('ul');
+    
+    this.slides = this.list?.children;
+    this.realSlides = [...this.slides];
+
+    if (!this.slides || !this.children?.length) {
+      return;
+    }
+
+    this.indexOffset = 0;
+
+    this.setupHandlers();
+
+    if (this.props.loop) {
+      this.cloneSlides();    
+    }
+
+    this.checkIfSliderIsNeeded();
+  }
+
+  //≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+  // ✅ Utility functions
+  //≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+
+  // Get the active slide by checking which is closest to the left of the parent
+  // If centered, check which is closest to the center of the parent
+  getActiveSlide(includeDistance = false) {    
+    let activeSlide = null;
+    let activeSlideDistance = null;
+
+    [...this.slides].forEach((slide, index) => {
+      const slideRect = slide.getBoundingClientRect();
+      const parentRect = this.list.getBoundingClientRect();
+
+      let distanceFromReference;
+
+      if (this.props.centered) {
+        const slideCenter = slideRect.left + slideRect.width / 2;
+        const parentCenter = parentRect.left + parentRect.width / 2;
+
+        distanceFromReference = Math.abs(slideCenter - parentCenter);
+      } else {
+        // Get margin-left of the first slide and use it as the offset of the parent position
+        const firstSlideMarginLeft = parseInt(window.getComputedStyle(this.slides[0]).marginLeft, 0);
+
+        distanceFromReference = (parentRect.left + firstSlideMarginLeft) - slideRect.left;
+
+        if (distanceFromReference < 0) {
+          distanceFromReference = distanceFromReference * -1;
+        }
+      }
+
+      if (activeSlideDistance === null || distanceFromReference < activeSlideDistance) {
+        activeSlide = index - this.indexOffset;
+        activeSlideDistance = distanceFromReference;
+      }
+    });
+
+    console.log({activeSlide});
+    if (includeDistance) {
+      return { activeSlide, activeSlideDistance };
+    }
+
+    return activeSlide;
+  }
+
+  scrollToSlide(index, behavior = 'smooth') {
+    index += this.indexOffset;
+
+    const slide = this.slides[index];
+
+    let positionToSlideTo;
+    if (this.props.centered) {
+      const offsetOfFirstSlide = this.slides[0].offsetLeft - this.slides[0].offsetWidth / 2;
+      positionToSlideTo = slide.offsetLeft - slide.offsetWidth / 2 - offsetOfFirstSlide + 10;
+    } else {
+      const offsetOfFirstSlide = this.slides[0].offsetLeft;
+      positionToSlideTo = slide.offsetLeft - offsetOfFirstSlide + 10;
+    }
+
+    this.list.scrollTo({
+      left: positionToSlideTo,
+      behavior,
+    });
+  }
+
+  cloneSlides() {
+    this.indexOffset = this.props.slidesToClone;
+
+    // Clone the last this.indexOffset slides and add them to the beginning of the list
+    for (let i = 0; i < this.indexOffset; i++) {
+      const lastSlide = this.realSlides[this.realSlides.length - 1 - i];
+      const clone = lastSlide.cloneNode(true);
+      clone.classList.add('fake');
+      this.list.insertBefore(clone, this.list.firstChild);
+    }
+
+    // Now, clone the first this.indexOffset slides and add them to the end
+    for (let i = 0; i < this.indexOffset; i++) {
+      const firstSlide = this.realSlides[i];
+      const clone = firstSlide.cloneNode(true);
+      clone.classList.add('fake');
+      this.list.appendChild(clone);
+    }
+
+    this.slides = this.list.children;
+    this.realSlides = [...this.slides].filter(slide => !slide.classList.contains('fake'));
+
+    // Set tabindex of all fake slides and their children to -1
+    const fakeSlides = [...this.slides].filter(slide => slide.classList.contains('fake'));
+    fakeSlides.forEach(slide => {
+      slide.setAttribute('tabindex', '-1');
+      [...slide.querySelectorAll('*')].forEach(child => child.setAttribute('tabindex', '-1'));
+    });
+
+    setTimeout(() => {
+      this.scrollToSlide(0, 'instant');
+    }, 100);
+  }
+
+  // Disable prev/next buttons if the width of the list is enough to fit all children
+  checkIfSliderIsNeeded() {    
+    if (this.props.centered || this.props.loop) {
+      return;
+    }
+
+    // Check if list has pseudo elements, and get their widths
+    const pseudoBefore = window.getComputedStyle(this.list, ':before');
+    const pseudoAfter = window.getComputedStyle(this.list, ':after');
+
+    const beforeWidth = parseInt(pseudoBefore.width, 10) || 0;
+    const afterWidth = parseInt(pseudoAfter.width, 10) || 0;
+
+    const scrollWidth = this.list.scrollWidth - beforeWidth - afterWidth;
+    const offsetWidth = this.list.offsetWidth;
+
+    // If the scrollWidth is greater than the offsetWidth, we need to show the prev and next buttons
+    if (scrollWidth > offsetWidth) {
+      this.list.classList.remove('no-scroll');
+      const activeSlide = this.getActiveSlide();
+      if (this.prev && (activeSlide > 0 || this.props.loop)) {
+        this.prev.removeAttribute('disabled');
+      }
+      if (this.next && (activeSlide < this.slides.length - 1 || this.props.loop)) {
+        this.next.removeAttribute('disabled');
+      }
+    } else {
+      this.list.classList.add('no-scroll');
+      if (this.prev) {
+        this.prev.setAttribute('disabled', 'true');
+      }
+      if (this.next) {
+        this.next.setAttribute('disabled', 'true');
+      }
+    }
+  }
+
+  //≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+  // ✅ Setup Handlers
+  //≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
+
+  setupHandlers() {
+    if (this.prev) {
+      this.prev.addEventListener('click', () => {
+        const activeSlide = this.getActiveSlide();
+        if (activeSlide + this.indexOffset > 0) {
+          this.scrollToSlide(activeSlide - 1);
+        }
+      });
+    }
+
+    if (this.next) {
+      this.next.addEventListener('click', () => {
+        const activeSlide = this.getActiveSlide();
+        if (activeSlide < this.slides.length - 1) {
+          this.scrollToSlide(activeSlide + 1);
+        }
+      });
+    }
+
+    // On window resize, run checkIfSliderIsNeeded
+    window.addEventListener('resize', () => {
+      this.checkIfSliderIsNeeded();
+    });
+
+    this.list.addEventListener('scroll', () => {
+      const activeSlideRes = this.getActiveSlide(true);
+      const { activeSlide, activeSlideDistance } = activeSlideRes;
+
+      if (this.props.loop && activeSlide < 0) {
+        if (activeSlideDistance < 10) {
+          // If activeSlide is negative, it means we should loop to last real slide
+          this.scrollToSlide(this.realSlides.length - 1, 'instant');
+        }
+      } else if (this.props.loop && activeSlide > this.realSlides.length - 1) {
+        if (activeSlideDistance < 10) {
+          // If activeSlide is greater than the number of real slides, it means we should loop to the beginning
+          this.scrollToSlide(0, 'instant');
+        }
+      } else {        
+        if (this.currentSlideIndicator) {
+          this.currentSlideIndicator.textContent = activeSlide + 1;
+        }
+        
+        // Debounce the event
+        if (this.scrollTimeout) {
+          clearTimeout(this.scrollTimeout);
+        }
+        this.scrollTimeout = setTimeout(() => {
+          this.dispatchEvent(new CustomEvent(EnhancedCssSlider.events.slideChanged, { detail: { activeSlide } }));          
+        }, 200);
+      }
+
+      if (!this.props.loop) {
+        if (activeSlide === 0) {
+          this.prev?.setAttribute('disabled', 'true');
+        } else {
+          this.prev?.removeAttribute('disabled');
+        }
+        if (activeSlide === this.slides.length - 1) {
+          this.next?.setAttribute('disabled', 'true');
+        } else {
+          this.next?.removeAttribute('disabled');
+        }
+      }
+    });
+  }
+}
+
+EnhancedCssSlider.register();
